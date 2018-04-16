424b757a8103292c12074aaa42533ce48e92fa23
Will Eccles <ghostyx123@gmail.com>
2018-04-16 11:02:27 -0400

    toolbar added

--- app/src/main/java/me/willeccles/weathermaster/MainActivity.java
+++ app/src/main/java/me/willeccles/weathermaster/MainActivity.java
@@ -1,10 +1,10 @@
 package me.willeccles.weathermaster;
 
-import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+		setSupportActionBar(myToolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.toolbar_layout, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				// show the settings (if needed, if not we can remove this option)
+				return true;
+
+			case R.id.action_map:
+				// show the map view
+				return true;
+
+			default:
+				// If we got here, the user's action was not recognized.
+				// Invoke the superclass to handle it.
+				return super.onOptionsItemSelected(item);
+
+		}
 	}
 
 	/**
