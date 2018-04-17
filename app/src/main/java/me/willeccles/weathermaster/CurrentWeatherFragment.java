20c49c2b72d726fa6bcd7d72ab93435ca843251c
Will Eccles <ghostyx123@gmail.com>
2018-04-17 11:54:19 -0400

    changed some names and did some stuff

--- app/src/main/java/me/willeccles/weathermaster/CurrentWeatherFragment.java
+++ app/src/main/java/me/willeccles/weathermaster/CurrentWeatherFragment.java
@@ -12,24 +12,30 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link DetailFragment.OnFragmentInteractionListener} interface
+ * {@link CurrentWeatherFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link DetailFragment#newInstance} factory method to
+ * Use the {@link CurrentWeatherFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailFragment extends Fragment {
+public class CurrentWeatherFragment extends Fragment {
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+	public static final String ARG_PARAM_CITY = "param1";
+	public static final String ARG_PARAM_STATUS = "param2";
+	public static final String ARG_PARAM_TEMP = "param3";
+	public static final String ARG_PARAM_HIGH = "param4";
+	public static final String ARG_PARAM_LOW = "param5";
 
 	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
+	private String paramCity;
+	private String paramStatus;
+	private int paramTemp;
+	private int paramHigh;
+	private int paramLow;
 
 	private OnFragmentInteractionListener mListener;
 
-	public DetailFragment() {
+	public CurrentWeatherFragment() {
 		// Required empty public constructor
 	}
 
@@ -37,16 +43,22 @@ public class DetailFragment extends Fragment {
 	 * Use this factory method to create a new instance of
 	 * this fragment using the provided parameters.
 	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment DetailFragment.
+	 * @param location The name of the location.
+	 * @param status The status of the weather (raining, snowing, etc)
+	 * @param temp The current temperature.
+	 * @param high The high temperature of the day.
+	 * @param low The low temperature for the day.
+	 * @return A new instance of fragment CurrentWeatherFragment.
 	 */
 	// TODO: Rename and change types and number of parameters
-	public static DetailFragment newInstance(String param1, String param2) {
-		DetailFragment fragment = new DetailFragment();
+	public static CurrentWeatherFragment newInstance(String location, String status, int temp, int high, int low) {
+		CurrentWeatherFragment fragment = new CurrentWeatherFragment();
 		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
+		args.putString(ARG_PARAM_CITY, location);
+		args.putString(ARG_PARAM_STATUS, status);
+		args.putInt(ARG_PARAM_TEMP, temp);
+		args.putInt(ARG_PARAM_HIGH, high);
+		args.putInt(ARG_PARAM_LOW, low);
 		fragment.setArguments(args);
 		return fragment;
 	}
@@ -55,8 +67,11 @@ public class DetailFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
+			paramCity = getArguments().getString(ARG_PARAM_CITY);
+			paramStatus = getArguments().getString(ARG_PARAM_STATUS);
+			paramTemp = getArguments().getInt(ARG_PARAM_TEMP);
+			paramHigh = getArguments().getInt(ARG_PARAM_HIGH);
+			paramLow = getArguments().getInt(ARG_PARAM_LOW);
 		}
 	}
 
@@ -64,7 +79,7 @@ public class DetailFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_detail, container, false);
+		return inflater.inflate(R.layout.fragment_current_weather, container, false);
 	}
 
 	// TODO: Rename method, update argument and hook method into UI event
