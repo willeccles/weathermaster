160640540172268547ef38dc8e1b2f00e2ee71a7
Will Eccles <ghostyx123@gmail.com>
2018-04-23 12:39:05 -0400

    added some sql stuff

--- app/src/main/java/me/willeccles/weathermaster/FavoritesHelper.java
+++ app/src/main/java/me/willeccles/weathermaster/FavoritesHelper.java
@@ -11,52 +11,67 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class FavoritesHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "mylist.db";
-    public static final String TABLE_NAME = "mylist_data";
-    public static final String COL1 = "ID";
-    public static final String COL2 = "NAME";
-    public FavoritesHelper(Context context) {
-        super(context,DATABASE_NAME,null,1);
-    }
+	public static final String DATABASE_NAME = "mylist.db";
+	public static final String TABLE_NAME = "mylist_data";
+	public static final String COL1 = "ID";
+	public static final String COL2 = "TITLE";
+	public static final String COL3 = "CITYID";
 
-    //Creates the table for the dara
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "NAME TEXT)";
-        db.execSQL(createTable);
-    }
+	public FavoritesHelper(Context context) {
+		super(context,DATABASE_NAME,null,1);
+	}
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-    }
+	//Creates the table for the dara
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				"TITLE TEXT, CITYID INT)";
+		db.execSQL(createTable);
+	}
 
-    //adds the data by taking the values and putting them into the database
-    public boolean saveFavorite(String item1) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL2, item1);
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+	}
 
-        long result = db.insert(TABLE_NAME, null, contentValues);
+	//adds the data by taking the values and putting them into the database
+	public boolean saveFavorite(String name, int cityID) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor prevdata = db.rawQuery(String.format("SELECT * FROM %s WHERE %s='%s' OR %s=%d;", TABLE_NAME, COL2, name, COL3, cityID), null);
+		if (prevdata.getCount() != 0) return false;
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(COL2, name);
+		contentValues.put(COL3, cityID);
 
-        if(result == -1) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+		long result = db.insert(TABLE_NAME, null, contentValues);
 
-    //get the contents tp then be able to display the data
-    public Cursor getFavorites() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-        return data;
-    }
+		if(result == -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    //delete all the data in the db
-    public void removeFavorite() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("delete from " +TABLE_NAME);
-    }
+	//get the contents tp then be able to display the data
+	public Cursor getFavorites() {
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+		return data;
+	}
+
+	public Cursor getFavoritesByName(String name) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		return db.rawQuery(String.format("SELECT * FROM %s WHERE %s='%s';", TABLE_NAME, COL2, name), null);
+	}
+
+	public void removeFavorite(String name, int cityID) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.execSQL(String.format("DELETE FROM %s WHERE %s='%s' AND %s=%d;", TABLE_NAME, COL2, name, COL3, cityID));
+	}
+
+	//delete all the data in the db
+	public void removeAllFavorites() {
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.execSQL("DELETE FROM " + TABLE_NAME);
+	}
 }
