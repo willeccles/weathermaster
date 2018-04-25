4639bc01a9fa6d9e1d4d27ab02cd9a42198f6b45
Will Eccles <ghostyx123@gmail.com>
2018-04-25 14:14:17 -0400

    added share doohicky

--- app/src/main/java/me/willeccles/weathermaster/DetailActivity.java
+++ app/src/main/java/me/willeccles/weathermaster/DetailActivity.java
@@ -1,14 +1,17 @@
 package me.willeccles.weathermaster;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +27,7 @@ public class DetailActivity extends AppCompatActivity {
 	public static final int FORECAST = 1;
 	private boolean isFavorite = false;
 	private Bundle weatherBundle;
+	private Intent shareIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +62,28 @@ public class DetailActivity extends AppCompatActivity {
 			}
 			transaction.commit();
 		}
+
+		// the intent to share from
+		shareIntent = new Intent();
+		shareIntent.setAction(Intent.ACTION_SEND);
+		String shareStr;
+		if (weatherBundle.getInt(TYPE) == CURRENT) {
+			shareStr = String.format("It's %.1fº in %s right now!", weatherBundle.getDouble("temp"), weatherBundle.getString("location"));
+		} else {
+			shareStr = String.format("In %s today, the low will be %.1fº and the high will be %.1fº!", weatherBundle.getBundle("day0").getString("location"), weatherBundle.getBundle("day0").getDouble("temp_min"), weatherBundle.getBundle("day0").getDouble("temp_max"));
+		}
+		shareIntent.putExtra(Intent.EXTRA_TEXT, shareStr);
+		shareIntent.setType("text/plain");
 	}
 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.detail_toolbar, menu);
+
 		if (isFavorite)
 			menu.findItem(R.id.action_favorite).setIcon(getDrawable(R.drawable.ic_favorite_white_24dp));
+
 		return true;
 	}
 
@@ -92,6 +110,10 @@ public class DetailActivity extends AppCompatActivity {
 			case android.R.id.home:
 				// this circumvents the parent activity so that you can always go back to the previous display
 				finish();
+				return true;
+
+			case R.id.action_share:
+				startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)));
 				return true;
 
 			default:
