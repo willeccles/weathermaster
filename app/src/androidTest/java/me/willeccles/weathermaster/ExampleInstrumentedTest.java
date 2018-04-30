71111f2a669b0aefe355809f96392007139c549e
Will Eccles <ghostyx123@gmail.com>
2018-04-30 11:20:44 -0400

    we need to run tests but need to buy a cable first

--- app/src/androidTest/java/me/willeccles/weathermaster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/me/willeccles/weathermaster/ExampleInstrumentedTest.java
@@ -2,11 +2,24 @@ package me.willeccles.weathermaster;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.Espresso.*;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -23,4 +36,26 @@ public class ExampleInstrumentedTest {
 
 		assertEquals("me.willeccles.weathermaster", appContext.getPackageName());
 	}
+
+	@Test
+	public void testCurrentWeather() {
+		onView(withId(R.id.zipCode)).perform(typeText("06084"));
+		onView(withId(R.id.buttonCurrent)).perform(click());
+		onView(withId(R.id.my_toolbar)).check(matches(withToolbarTitle(is("Hartford"))));
+	}
+
+	// for matching the text in a toolbar
+	private static Matcher<Object> withToolbarTitle(
+			final Matcher<String> textMatcher) {
+		return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+			@Override public boolean matchesSafely(Toolbar toolbar) {
+				return textMatcher.matches(toolbar.getTitle());
+			}
+			@Override public void describeTo(Description description) {
+				description.appendText("with toolbar title: ");
+				textMatcher.describeTo(description);
+			}
+		};
+	}
+
 }
